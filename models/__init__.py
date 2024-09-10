9128889065bef04cf3fb138cc12a9f3338a4f37d
RuizhouLiu <573266068@qq.com>
2024-09-10 07:20:27 +0000

    Third commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,3 @@
-from .complex import *
-from .euclidean import *
 from .hyperbolic import *
-from .our_model import *
 
-all_models = EUC_MODELS + OUR_MODELS + COMPLEX_MODELS + HYP_MODELS
+all_models = HYP_MODELS
